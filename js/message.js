03a92897feca81f57d7d385f1f81cf61a3934575
ai-cuicui <ai_hasana@163.com>
2018-10-19 18:23:34 +0800

    1.版本上传已修改   2.平台消息遗留：标题拼接、未读消息提示

--- js/message.js
+++ js/message.js
@@ -1,53 +1,50 @@
-//获取当前时间
-var date = new Date();
-var year = date.getFullYear();
-var month = date.getMonth() + 1;
-var day = date.getDate();
-if (month < 10) {month = "0" + month;}
-if (day < 10) {day = "0" + day;}
-var nowDate = year + "-" + month + "-" + day;
-
 var datas = {
     "start": 0,
-    "limit": 34
+    "limit": 12
 };
-Ajax.call('/apis/api/method/iot.user_api.device_activity', JSON.stringify(datas), message, 'POST', 'JSON', 'JSON');
+Ajax.call('/apis/api/method/iot.user_api.device_activity',JSON.stringify(datas), message, 'POST', 'JSON', 'JSON');
 function message(req){
+    console.log(req.message)
     var list = req.message;
-    //判断有没有最近消息
-    if(list.length == 0 ){
+    var posed = [];
+    for (var j=0;j<list.length;j++){
+        console.log(list[j].disposed);
+        if(list[j].disposed == 0){
+            posed.push(list[j]);
+        }
+    }  //把所有未读消息集合
+    // console.log(posed);
+    if(posed.length == 0){
+        console.log('暂时没有未读信息');
+        $('.have').hide();
+        $('.no_have').show();
         $('.redDot').hide();
     }else{
+        console.log('有未读信息');
+        $('.have').show();
+        $('.no_have').hide();
         $('.redDot').show();
-    }
-    //渲染列表
-    var arr1 = [];
-    for (var j=0;j<list.length;j++){
-        if(list[j].disposed == 0){
-            arr1.push(list[j]);
+        //渲染列表
+        for (var i=0;i<12;i++){
+            $('.list_box').append(
+                '<li class="box_li">' +
+                '<span>'+posed[i].subject.substr(0,18)+'...'+ $.parseJSON(posed[i].message).device_status +'!</span>' +
+                '<span class="message_id" style="display: none;">'+posed[i].name+'</span>' +
+                '<span class="message_ago" title="'+posed[i].creation+'"></span>' +
+                '</li>'
+            );
+            $('.message_ago').eq(i).timeago();
+            $('.box_li')[i].index = i;
+            $('.box_li')[i].onclick = function(){
+                var name = $('.message_id').eq(this.index).text();
+                location.href = 'platform_details.html?name=' + name;
+            }
         }
-    }
-    console.log('-------------------')
-    console.log(arr1)
-    for (var i=0;i<12;i++){
-        $('.list_box').append(
-            '<li class="box_li">' +
-            '<span>'+arr1[i].subject.substr(0,18)+'...'+ $.parseJSON(arr1[i].message).device_status +'!</span>' +
-            '<span class="message_id" style="display: none;">'+arr1[i].name+'</span>' +
-            '<span class="message_ago" title="'+arr1[i].creation+'"></span>' +
-            '</li>'
-        );
+        var ago = $('.message_ago');
+        console.log(ago);
+        for(var i=0;i<ago.length;i++){
+            console.log(ago[i])
+        }
         $('.message_ago').eq(i).timeago();
-        $('.box_li')[i].index = i;
-        $('.box_li')[i].onclick = function(){
-            var name = $('.message_id').eq(this.index).text();
-            location.href = 'platform_details.html?name=' + name;
-        }
     }
-    var ago = $('.message_ago');
-    console.log(ago);
-    for(var i=0;i<ago.length;i++){
-        console.log(ago[i])
-    }
-    $('.message_ago').eq(i).timeago();
 }
